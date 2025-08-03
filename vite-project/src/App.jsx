db05b12e96e755dcfe78e574f128c38d017b9085
Gaurav Singh <gaurav.singh.971999@gmail.com>
2025-08-03 22:30:09 +0530

    learning custom render and  rendering of functions and objects

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,13 +2,12 @@ import Chai from "./chai"
 
 
 function App() {
-
+  const Username = "chai aur Code"
   return (
     <>
     <Chai/>
-    <h1>chai aur react</h1>
-      <p>test para</p>
-    
+    <h1>chai aur react {Username}</h1>
+    <p>test para</p>
     </>
   )
 }
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,9 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
- 
-    <App />
+
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App | Gaurav Singh</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+
+)
+
+const anotherUser = "chai aur react"
+
+
+const reactElement = React.createElement(
+
+    'a',
+    {href: 'https://google.com',target:'_blank'},
+    'Click me to visit Google',
+    anotherUser
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+    // <App />
+    // <MyApp />
+    // MyApp()
+
+    // anotherElement
+    reactElement
 
 )
